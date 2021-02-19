db39703a25daffc55c603a0dd9d186205c604780
Ankita <ankitakashyap121@gmail.com>
2021-02-20 01:40:38 +0530

    Create code.py

---
+++ ankita/code.py
@@ -0,0 +1,32 @@
+print("Let's get started")
+#DATATYPES
+
+#NUMERIC TYPE
+x=20.4
+y=48.9
+z="ankita"
+print(x,y,z)
+
+#TYPE
+print(type(z))
+
+#COMPLEX TYPE
+d=3+5j
+print(d)
+print(type(d))
+
+"""Binary type:- 0B 
+Heaxadecimal type:- 0X
+Octadecimal type:- 0O"""
+
+#BINARY TYPE
+e=0B1000
+print(e)
+
+#HEXADECIMAL TYPE
+f=0X8
+print(f)
+
+#OCTADECIMAL TYPE
+g=0O10
+print(g)
