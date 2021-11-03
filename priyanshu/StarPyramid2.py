bde3da6126b168ea367122058b47b9380d389f29
Priyanshu Rajput <76838551+Priyanshu-rajput-01@users.noreply.github.com>
2021-11-03 22:33:02 +0530

    Update

--- priyanshu/StarPyramid2.py
+++ priyanshu/StarPyramid2.py
@@ -1,9 +1,7 @@
 def reverse(n):
     # number of spaces
     k = 2*n - 2
-    # outer loop to handle number of rows
     for i in range(0, n):
-
         for j in range(0, k):
             print(end=" ")
         k = k - 2
