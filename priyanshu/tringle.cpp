83e9063d2dc133add775d65b7eb22fb5c1a5b841
Priyanshu Rajput <76838551+Priyanshu-rajput-01@users.noreply.github.com>
2021-11-03 22:35:44 +0530

    Updated

--- priyanshu/tringle.cpp
+++ priyanshu/tringle.cpp
@@ -2,18 +2,13 @@
 def triangle(n):
 	# spaces
 	k = n - 1
-
 	for i in range(0, n):
-
 		for j in range(0, k):
 			print(end=" ")
-
 		k = k - 1
 		for j in range(0, i+1):
-
 			print("* ", end="")
-
 		print("\r")
-
-n = 10
-triangle(n)
+if __name__ == "main" :				
+	n = int(input("Enter the value of n : "))
+	triangle(n)
